ad1ea1ae06168821b2c272789d6253e0657716e4
Алексей Журавлев <dralex1982@gmail.com>
2023-10-30 14:29:12 +0200

    Correct

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,4 +13,4 @@ export const countOfEpisodes = 6;
 export const navItems = ['Home', 'About me', 'Star wars', 'Contact'];
 export const friends = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
 
-export const period = 1000 * 60 * 60 * 24 * 15; //миллисекунд в 15 днях
+export const period = 1000 * 60 * 60 * 24 * 30; //миллисекунд в 30 днях
