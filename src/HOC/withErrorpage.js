c2579cd9a8a63f58070040638b1f907e2fef4209
Алексей Журавлев <dralex1982@gmail.com>
2023-11-24 10:25:02 +0200

    Add HOC withErrorPage

--- src/HOC/withErrorpage.js
+++ src/HOC/withErrorpage.js
@@ -1,25 +1,18 @@
 import {useContext, useEffect} from "react";
-import {characters, defaultHero} from "../utils/constants";
+import {friends} from "../utils/constants";
 import {starWarsContext} from "../utils/starWarsContext";
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import ErrorPage from "../components/ErrorPage";
 
 export const withErrorpage = route => Component => props => {
 
     const {setHeroFromPath} = useContext(starWarsContext);
     let {heroId} = useParams();
-    const navigate = useNavigate()
 
     useEffect(() => {
-        if (!characters.includes(heroId)) {
-            navigate(`/${route}/*`);
-            console.log(`${route}`)
-            return;
-        } else
+        if (friends[heroId])
             setHeroFromPath(heroId);
     }, [heroId]);
-    console.log(heroId)
-    if (heroId === '*')
-        return <ErrorPage/>
-    else return <Component {...props}/>
+
+    return friends[heroId] ? <Component {...props}/> : <ErrorPage/>;
 }
